b5d8c3f954046f48405209e5c24c2aedcc0ea43f
vrinda kohli <vrindakohli0909@gmail.com>
2021-11-25 09:39:10 +0530

    first commit

---
+++ src/main/java/com/rapipay/newOTPGenerate/controller/MyController.java
@@ -0,0 +1,94 @@
+package com.rapipay.newOTPGenerate.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rapipay.newOTPGenerate.dto.RequestDto;
+import com.rapipay.newOTPGenerate.dto.ResponseDto;
+import com.rapipay.newOTPGenerate.entities.OTPEntities;
+import com.rapipay.newOTPGenerate.exception.InvalidChannel;
+import com.rapipay.newOTPGenerate.exception.InvalidEmail;
+import com.rapipay.newOTPGenerate.exception.InvalidMobile;
+import com.rapipay.newOTPGenerate.exception.InvalidOTPException;
+import com.rapipay.newOTPGenerate.exception.NoUserException;
+import com.rapipay.newOTPGenerate.exception.NullValueException;
+import com.rapipay.newOTPGenerate.exception.ReGenerateOTP;
+import com.rapipay.newOTPGenerate.exception.TimeStampExceededException;
+import com.rapipay.newOTPGenerate.services.OTPServiceInterface;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@RequestMapping("/api/v1.1/otp")
+@RestController
+@Api(value = "/api/v1.1/otp", tags = "otp generation")
+public class MyController implements ErrorController {
+	//private static final Logger LOGGER = LoggerFactory.getLogger(MyController.class);
+
+	@Autowired
+	ModelMapper modelMapper; //entity to DTO conversion
+	
+	@Autowired
+	private OTPServiceInterface otpService;
+	
+	/*@GetMapping("/genOtp")
+	public String generateOTP() {
+		return this.otpService.generateOTP();
+	}*/
+	private ResponseDto responseDto;
+		
+	@PostMapping(path="/addData")
+	@ApiOperation(value = "generate otp", notes = "input user data to generate otp")
+	public ApplicationResponseEntity<ResponseDto> addDataInDb( @RequestBody RequestDto requestDto) throws Exception {
+			try {
+				//dto to entity
+				OTPEntities otpEntities= modelMapper.map(requestDto, OTPEntities.class); //maps dto to otpentities
+				otpService.addData(otpEntities);
+				
+				//entity to dto in return
+				return new ApplicationResponseEntity<>("200", "successfull", modelMapper.map(otpEntities, ResponseDto.class));
+			} 
+			catch (InvalidEmail | InvalidMobile | InvalidChannel | ReGenerateOTP | NullValueException e) {
+				// TODO Auto-generated catch block
+				throw e;
+
+
+			}
+			
+			
+		
+		}
+	
+	@GetMapping("/validate/{user_id}/{otp}")
+	@ApiOperation(value = "validate otp", notes = "input email/sms ie. your user id and the otp to validate otp")
+
+	public ApplicationResponseEntity<ResponseDto>validateOTP(@PathVariable String user_id, @PathVariable String otp) {
+		//System.out.println(user_id+""+otp);
+	
+			try {
+				OTPEntities otpEntities=otpService.validateOtp(user_id, otp);
+				return new ApplicationResponseEntity<>("200", "succesfull", modelMapper.map(otpEntities, ResponseDto.class));
+			} catch (NoUserException | InvalidOTPException | TimeStampExceededException e) {
+				// TODO Auto-generated catch block
+				return new ApplicationResponseEntity<>("400", e.toString(), null);
+			}
+		
+	}
+	
+}
---
+++ src/main/java/com/rapipay/newOTPGenerate/services/OTPService.java
@@ -0,0 +1,170 @@
+package com.rapipay.newOTPGenerate.services;
+
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.google.common.base.Strings;
+import com.rapipay.newOTPGenerate.dao.OtpDao;
+import com.rapipay.newOTPGenerate.entities.OTPEntities;
+import com.rapipay.newOTPGenerate.exception.InvalidChannel;
+import com.rapipay.newOTPGenerate.exception.InvalidEmail;
+import com.rapipay.newOTPGenerate.exception.InvalidMobile;
+import com.rapipay.newOTPGenerate.exception.InvalidOTPException;
+import com.rapipay.newOTPGenerate.exception.NoUserException;
+import com.rapipay.newOTPGenerate.exception.NullValueException;
+import com.rapipay.newOTPGenerate.exception.ReGenerateOTP;
+import com.rapipay.newOTPGenerate.exception.TimeStampExceededException;
+
+import net.bytebuddy.implementation.bytecode.Throw;
+
+@Service
+public class OTPService implements OTPServiceInterface {
+	private static final Logger LOGGER = LoggerFactory.getLogger(OTPService.class);
+
+	@Value("${app.generate}")
+	public int x;
+	@Value("${app.validate}")
+	public int y;
+
+	@Autowired
+	private OtpDao otpDao;
+
+	@Autowired
+	private JavaMailSender sender;   //javamailsender is synchronous
+	//asynchronous means running the blocks parallely
+
+	@Override
+	public String generateOTP() {
+		int otp;
+		Random random = new Random();
+		otp = 100000 + random.nextInt(900000);
+		return String.valueOf(otp);
+
+	}
+
+	@Override
+	public OTPEntities addData(OTPEntities otpEntities)
+			throws InvalidEmail, InvalidMobile, InvalidChannel, ReGenerateOTP, NullValueException {
+
+		otpEntities.setOtp(generateOTP());
+		LOGGER.info("Entered generate otp section");
+
+		// to ensure otp is not generated before x minute
+		OTPEntities oe = otpDao.findById(otpEntities.getUser_id()).orElse(null);
+		//System.out.println(x + " hello");
+//		if (Strings.isNullOrEmpty(otpEntities.getChannelName())) {
+//			LOGGER.error("channel is null for the input "+otpEntities.getChannelName());
+//			throw new NullValueException("channel cant be null");
+//		}
+//		if (Strings.isNullOrEmpty(otpEntities.getUser_id())) {
+//			LOGGER.error("user id is null. entered user id is: "+otpEntities.getUser_id());
+//			throw new NullValueException("user id cant be null");
+//		}
+//		if (otpEntities.getOrder_id() == 0) {
+//			LOGGER.error("order id is null. entered order id is: "+otpEntities.getOrder_id());
+//			throw new NullValueException("order id cant be null");
+//		}
+		
+		if (oe != null) {
+			if (((System.currentTimeMillis() - oe.getTimeStamp()) / 60000) < x) {
+				LOGGER.error("can not generate otp again. otp regenerate requested at "+ System.currentTimeMillis());
+				throw new ReGenerateOTP();
+			}
+		}
+		if (otpEntities.getChannelName().equalsIgnoreCase("email")) {
+			if (!checkEmail(otpEntities.getUser_id())) {
+				LOGGER.error("email is invalid. entered email id is: "+otpEntities.getUser_id());
+				throw new InvalidEmail();
+			} else {
+				LOGGER.info("mail sent");
+				System.out.println(otpEntities.getUser_id());
+				sendEmail(otpEntities.getUser_id(), "otp is: " + otpEntities.getOtp(), "otp");
+
+			}
+
+		} else if (otpEntities.getChannelName().equalsIgnoreCase("sms")) {
+			if (!otpEntities.getUser_id().matches("[0-9]+")) {
+				LOGGER.error("invalid mobile number. entered number is: "+ otpEntities.getUser_id());
+				throw new InvalidMobile();
+			}
+			if (otpEntities.getUser_id().length() != 10) {
+				LOGGER.error("mobile number not 10 digits. entered mobile number is: "+otpEntities.getUser_id());
+				throw new InvalidMobile();
+			}
+
+		} else {
+			LOGGER.error("channel name not valid. entered channel name is: "+otpEntities.getChannelName());
+			throw new InvalidChannel();
+		}
+
+		System.out.println(otpEntities.getOtp());
+		otpEntities.setTimeStamp();
+		otpDao.save(otpEntities);
+		LOGGER.info("entries saved");
+		return otpEntities;
+
+	}
+
+	@Override
+	public OTPEntities validateOtp(String uId, String otp)
+			throws NoUserException, InvalidOTPException, TimeStampExceededException {
+		// TODO Auto-generated method stub
+		LOGGER.info("entered validate otp section");
+		long ts = System.currentTimeMillis();
+		OTPEntities otpEntities = otpDao.findById(uId).orElseThrow(() -> new NoUserException());
+		// String channelName= otpEntities.getChannelName();
+		// System.out.println(otpEntities);
+		if ((ts - otpEntities.getTimeStamp()) / 60000 < y) {
+			if (otp.equals(otpEntities.getOtp())) {
+				LOGGER.info("otp verified succesfully");
+				System.out.println("otp verified correctly");
+				return otpEntities;
+			} else {
+				LOGGER.error("otp doesnt match. entered otp is: "+otp);
+				throw new InvalidOTPException();
+			}
+
+		} else {
+			LOGGER.error("time stamp exceeded. time when otp is entered is: "+System.currentTimeMillis());
+
+			throw new TimeStampExceededException();
+
+		}
+
+	}
+
+	public boolean checkEmail(String email) {
+		Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+		Matcher mat = pattern.matcher(email);
+		if (!mat.matches()) {
+			return false;
+		}
+		return true;
+	}
+
+	
+	@Async
+	public void sendEmail(String toEmail, String body, String subject) {
+		LOGGER.info("mail sent to the user");
+		SimpleMailMessage simpleMessage = new SimpleMailMessage();
+		simpleMessage.setFrom("vrindakohli0909@gmail.com");
+		simpleMessage.setTo(toEmail);
+		simpleMessage.setText(body);
+		simpleMessage.setSubject(subject);
+		sender.send(simpleMessage);
+		System.out.println("mail sent");
+
+	}
+
+}
